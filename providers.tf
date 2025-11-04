ec1ff86a242c304eb261119d9b03020021d0c195
Abdelaziz-Gbr <abdelazizkhalid@outlook.com>
2025-11-04 16:06:23 +0200

    providers: :

---
+++ providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.9.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.default_region
+  zone    = var.default_zone
+}
+
+data "google_client_config" "default" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = "${var.project_id}-gke"
+  location = var.default_region
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+
+  token = data.google_client_config.default.access_token
+
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate
+  )
+}
